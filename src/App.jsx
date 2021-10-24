5ff110646f107a991c4120d040e5aaa1a2cccf45
burunda <wesotop@unal.edu.co>
2021-10-24 18:03:04 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import 'styles/styles.css';
+import 'styles/App.css';
 import Index from "pages/public/index";
 //import Perfil from "pages/perfil";
 import Clientes from "pages/admin/clientes";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,10 @@
-import GoogleLogin from 'react-google-login';
-
-
-
 
 const Header = () => {
-    const onsuccess=(respuesta)=>{
-        console.log('Inicio correcto');
-        alert('Inició correctamente');
-    }
-    const onfailure=(respuesta)=>{
-        console.log('Inicio fallido');
-        alert('Intenta de nuevo');
-    }
-
     return (
     <header className="header">
         <nav>
             <ul className='cabecera'>
-                <div>Logo</div>
                 <div className='titulo'>TeamApp<div>Trabajamos por su empresa</div>
-                </div>
-                <div>
-                    <button className='login'>
-                        <GoogleLogin 
-                        cdiventId="301885852798-pil9v0bsr0khvpjghrspk4nqhls89nij.apps.googleusercontent.com"
-                        buttonText="Iniciar Sesión"
-                        onSuccess={onsuccess}
-                        onFailure={onfailure}
-                        cookiePodivcy={'single_host_origin'}
-                        />
-                    </button>
                 </div>
             </ul>
         </nav>
